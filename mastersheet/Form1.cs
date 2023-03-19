94441a458797de32bd1b5bb82e4651c3e94c18ef
Yasser Dhia Hussein <yasser1994hp@yahoo.com>
2023-03-19 11:12:34 +0300

    3

--- mastersheet/Form1.cs
+++ mastersheet/Form1.cs
@@ -15,7 +15,7 @@ namespace mastersheet
 {
 
     
-    public partial class Form112 : Form
+    public partial class Form1123 : Form
 
     {
         MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;Initial Catalog='mastersheet';username=root;password=");
